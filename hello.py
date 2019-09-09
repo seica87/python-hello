e37e461f831d108e617a84a3e6e5301aa6b3ddf5
seica87 <seica87@gmail.com>
2019-09-09 15:06:45 +0900

    study

--- hello.py
+++ hello.py
@@ -1,4 +1,62 @@
-print('hello Py!!!!')
-print("hello world!!!!!!!!!!!!!!!!")
-print("git!!!!!!!!!!!!!!!!!!!!!!!")
+# # # # # # print('hello Py!!!!')
+# # # # # # print("hello world!!!!!!!!!!!!!!!!")
+# # # # # # print("git!!!!!!!!!!!!!!!!!!!!!!!")
 
+# # # # # # dup = 1
+# # # # # # while dup <=10:
+# # # # # #     print(dup)
+# # # # # #     dup = dup+1
+
+
+# # # # # prompt = """
+# # # # # 1. Add
+# # # # # 2. Del
+# # # # # 3. List
+# # # # # 4. Quit
+
+# # # # # Enter number : """
+# # # # # # number = 0
+# # # # # # while number != 4:
+# # # # # #     print(prompt)
+# # # # # #     number = int(input())
+
+
+# # # # # number = 0
+# # # # # while number != 4:
+# # # # #     print(prompt)
+# # # # #     number = int(input())
+# # # # #     print('*' * 79)
+# # # # #     print("your input nuber : {}".format(number))
+# # # # #     print('*' * 79)
+
+# # # # import time
+# # # # while True:
+# # # #     print("컨트롤+C를 눌러 빠져나오세요.")
+# # # #     time.sleep(1)
+
+# # # lst = [1,10]
+
+# # # for a in lst:
+# # #     print(a)
+# # scores = [100, 90, 80, 70, 100]
+
+# # tot=0
+
+# # for score in scores:
+# #     tot = tot + score
+
+# # avg = tot / len(scores)
+
+# # print("총점은 {}점 입니다.".format(tot))
+# # print("평균은 {}점 입니다.".format(avg))
+
+# for i in range(1,101):
+#     print(i)
+
+star = ("*")
+num = 0
+while num < 5:
+    num = num + 1
+    print(star*num)
+
+for a in range(1,6): print(star*a)
